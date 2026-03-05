d9ea789a9e59e2e2fbf2eab0b3e151e134f93549
Daniel Nobre <55868687+Dan-Nobre@users.noreply.github.com>
2026-03-05 14:13:24 -0300

    creating packages and models

--- app/src/main/java/com/example/meucarrinho/MainActivity.kt
+++ app/src/main/java/com/example/meucarrinho/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.meucarrinho.data.Category
+import com.example.meucarrinho.data.ShoppingItem
 import com.example.meucarrinho.ui.theme.MeuCarrinhoTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +21,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MeuCarrinhoTheme {
-                
+                val item = ShoppingItem(
+                    name = "Bananas",
+                    category = Category.HORTALICAS,
+                    quantity = 6
+                )
+                println("Item criado: ${item.name} | Categoria: ${item.category.label}")
             }
         }
     }
---
+++ app/src/main/java/com/example/meucarrinho/data/Category.kt
@@ -0,0 +1,12 @@
+package com.example.meucarrinho.data
+
+enum class Category(
+    val label: String
+) {
+    HORTALICAS("Hortaliças"),
+    LATICINIOS("Laticínios e Ovos"),
+    LIMPEZA("Itens de Limpeza"),
+    PADARIA("Padaria"),
+    PROTEINAS("Proteínas"),
+    LANCHES("Lanches")
+}
