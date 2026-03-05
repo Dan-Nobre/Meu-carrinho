af30b4826761688023bd2843cf73c93e4a804fde
Daniel Nobre <55868687+Dan-Nobre@users.noreply.github.com>
2026-03-05 14:21:53 -0300

    add Icons and Preview

--- app/src/main/java/com/example/meucarrinho/data/Category.kt
+++ app/src/main/java/com/example/meucarrinho/data/Category.kt
@@ -1,6 +1,14 @@
 package com.example.meucarrinho.data
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.BakeryDining
+import androidx.compose.material.icons.filled.Cookie
+import androidx.compose.material.icons.filled.Eco
+import androidx.compose.material.icons.filled.Egg
+import androidx.compose.material.icons.filled.LocalLaundryService
+import androidx.compose.material.icons.filled.SetMeal
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import com.example.meucarrinho.ui.theme.BluePrimary
 import com.example.meucarrinho.ui.theme.BlueSecondary
 import com.example.meucarrinho.ui.theme.BrownPrimary
@@ -16,37 +24,44 @@ import com.example.meucarrinho.ui.theme.RedSecondary
 
 enum class Category(
     val label    : String,
+    val icon     : ImageVector,
     val primary  : Color,
     val secondary: Color,
 ) {
     HORTALICAS(
-    label     = "Hortaliças",
-    primary   = GreenPrimary,
-    secondary = GreenSecondary,
+        label     = "Hortaliças",
+        icon      = Icons.Default.Eco,
+        primary   = GreenPrimary,
+        secondary = GreenSecondary,
     ),
     LATICINIOS(
-    label     = "Laticínios e Ovos",
-    primary   = OrangePrimary,
-    secondary = OrangeSecondary,
+        label     = "Laticínios e Ovos",
+        icon      = Icons.Default.Egg,
+        primary   = OrangePrimary,
+        secondary = OrangeSecondary,
     ),
     LIMPEZA(
-    label     = "Itens de Limpeza",
-    primary   = BluePrimary,
-    secondary = BlueSecondary,
+        label     = "Itens de Limpeza",
+        icon      = Icons.Default.LocalLaundryService,
+        primary   = BluePrimary,
+        secondary = BlueSecondary,
     ),
     PADARIA(
-    label     = "Padaria",
-    primary   = BrownPrimary,
-    secondary = BrownSecondary,
+        label     = "Padaria",
+        icon      = Icons.Default.BakeryDining,
+        primary   = BrownPrimary,
+        secondary = BrownSecondary,
     ),
     PROTEINAS(
-    label     = "Proteínas",
-    primary   = RedPrimary,
-    secondary = RedSecondary,
+        label     = "Proteínas",
+        icon      = Icons.Default.SetMeal,
+        primary   = RedPrimary,
+        secondary = RedSecondary,
     ),
     LANCHES(
-    label     = "Lanches",
-    primary   = PurplePrimary,
-    secondary = PurpleSecondary,
+        label     = "Lanches",
+        icon      = Icons.Default.Cookie,
+        primary   = PurplePrimary,
+        secondary = PurpleSecondary,
     ),
 }
