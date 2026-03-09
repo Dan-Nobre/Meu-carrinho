997ca44d3d43096accdecee26260c225cda307d5
Daniel Nobre <55868687+Dan-Nobre@users.noreply.github.com>
2026-03-09 14:18:29 -0300

    setting state remembrance behavior

--- app/src/main/java/com/example/meucarrinho/ui/screens/AddItemScreen.kt
+++ app/src/main/java/com/example/meucarrinho/ui/screens/AddItemScreen.kt
@@ -2,22 +2,32 @@ package com.example.meucarrinho.ui.screens
 
 import android.annotation.SuppressLint
 import android.widget.Space
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -30,6 +40,9 @@ import com.example.meucarrinho.ui.theme.MeuCarrinhoTheme
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddItemScreen() {
+
+    var itemName by remember { mutableStateOf("") }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -65,7 +78,31 @@ fun AddItemScreen() {
             )
         },
     ) { paddingValues ->
-        Spacer(Modifier.height(8.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(horizontal = 16.dp, vertical = 8.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+        ) {
+
+            // Campo de nome
+            Column {
+                Text(
+                    text       = "Nome do Item",
+                    fontWeight = FontWeight.Bold,
+                )
+                Spacer(Modifier.height(8.dp))
+                OutlinedTextField(
+                    value           = itemName,
+                    onValueChange   = { itemName = it },
+                    placeholder     = { Text("Suco de Laranja...") },
+                    singleLine      = true,
+                    modifier        = Modifier.fillMaxWidth(),
+                    shape           = RoundedCornerShape(12.dp),
+                )
+            }
+        }
     }
 }
 
